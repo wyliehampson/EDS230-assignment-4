d02afbbdf0d57a59be71077c79da2326235b44f1
Brianna Baker <brirose.baker@gmail.com>
2022-04-21 10:45:12 -0700

    finished documentation

--- R/atm_conductance.R
+++ R/atm_conductance.R
@@ -2,14 +2,14 @@
 
 #' Atmospheric Conductance Model
 #' Function to estimate atmospheric conductance based on vegetation height
-#' Takes vegetation height, wind speed and partial pressure
+#' Takes vegetation height, wind speed and k coefficients
 #'
 #' @param height vegetation height (cm)
-#' @param k_d
-#' @param k_0
+#' @param k_d unitless coefficient
+#' @param k_0 unitless coefficient
 #' @param windspeed wind speed (cm/s)
 #'
-#' @return atmospheric conductance (UNITS)
+#' @return atmospheric conductance (cm/s)
 #' @export
 #'
 #' @examples
