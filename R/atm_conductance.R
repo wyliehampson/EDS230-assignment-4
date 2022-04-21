73d4c9d7c7a5fdb372fcac2c76398f926d6e5696
wyliehampson <whampson@bren.ucsb.edu>
2022-04-21 10:47:49 -0700

    Did the workflow.

--- R/atm_conductance.R
+++ R/atm_conductance.R
@@ -1,5 +1,7 @@
 atm_conductance <- function(height, k_d = 0.7, k_0 = 0.1, windspeed) {
   
-  conductance <- windspeed / (6.25 * (log(((h + 200) - (k_d * height)) / (height * k_0)) ^ 2))
+  conductance <- windspeed / (6.25 * (log(((height + 200) - (k_d * height)) / (height * k_0)) ^ 2))
+  
+  return(conductance)
   
 }
