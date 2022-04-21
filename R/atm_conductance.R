0918ef0baec60449df166ee5436a9b501fdfebca
Brianna Baker <brirose.baker@gmail.com>
2022-04-21 10:08:48 -0700

    added *most* of function documentation, need what k is + final units

--- R/atm_conductance.R
+++ R/atm_conductance.R
@@ -1,3 +1,21 @@
+
+
+#' Atmospheric Conductance Model
+#' Function to estimate atmospheric conductance based on vegetation height
+#' Takes vegetation height, wind speed and partial pressure
+#'
+#' @param height vegetation height (cm)
+#' @param k_d
+#' @param k_0
+#' @param windspeed wind speed (cm/s)
+#'
+#' @return atmospheric conductance (UNITS)
+#' @export
+#'
+#' @examples
+
+
+
 atm_conductance <- function(height, k_d = 0.7, k_0 = 0.1, windspeed) {
   
   conductance <- windspeed / (6.25 * (log(((h + 200) - (k_d * height)) / (height * k_0)) ^ 2))
